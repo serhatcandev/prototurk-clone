7cb45838d8407bcc51784b3d502b9a57d8630421
Serhat Can <85739464+serhatc4n@users.noreply.github.com>
2022-10-16 00:38:39 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        prototurk: "#32445a",
+        twitter: "#1da1f2",
+      },
+    },
+  },
+  plugins: [],
+};
